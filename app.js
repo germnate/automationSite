1261606dc2d27be2dda4939aedbc47dab5d00b3a
germnate <bustamanten2@gmail.com>
2020-05-20 17:02:43 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.listen(process.env.PORT || port, function(){
+  console.log("server listening on: " + port );
+});
